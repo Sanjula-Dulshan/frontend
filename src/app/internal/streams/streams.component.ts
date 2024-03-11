5fb7c0fa5fe8d3002b7251304b3ae980124e9999
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-11 19:38:48 +0530

    display stream list

--- src/app/internal/streams/streams.component.ts
+++ src/app/internal/streams/streams.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StreamsService } from '../streams.service';
 
 @Component({
   selector: 'app-streams',
   templateUrl: './streams.component.html',
-  styleUrls: ['./streams.component.scss']
+  styleUrls: ['./streams.component.scss'],
 })
-export class StreamsComponent {
+export class StreamsComponent implements OnInit {
+  streams: any[] = [];
 
+  constructor(private streamService: StreamsService) {}
+
+  ngOnInit(): void {
+    this.streamService.getStreams().subscribe((data) => {
+      this.streams = data;
+    });
+  }
 }

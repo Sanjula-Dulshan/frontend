b46b23b575a14b4337362753392f136b4cd81942
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-11 19:05:04 +0530

    init

---
+++ src/app/internal/internal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StreamsComponent } from './streams/streams.component';
+
+
+
+@NgModule({
+  declarations: [
+    StreamsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InternalModule { }
